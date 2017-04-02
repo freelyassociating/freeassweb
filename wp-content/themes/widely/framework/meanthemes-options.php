7f8668d1b164632470c63f80e3ebb81591c10bb8
freelyassociating <brian@brians-iMac.local>
2017-04-02 17:44:43 +0100

    Update 0204

---
+++ wp-content/themes/widely/framework/meanthemes-options.php
@@ -0,0 +1,250 @@
+/*
+*/
+@import url(//fonts.googleapis.com/css?family=Raleway|Bitter);
+body,
+input,
+textarea { font-family: "Bitter", "cursive"; }
+
+
+
+
+
+h1, .content.archive-content h2, .lead .title, .site-title, .tagline, header nav, .mean-nav {
+font-family: "Raleway", "cursive";}
+
+
+/* #######################################################################
+
+	Theme CSS Options
+
+####################################################################### */
+
+/* Background Colours */
+
+/* Content Wrapper - Background covers entire page, use this for image tiles + full size cover too */
+section#main {
+background: #fafafa; 
+}
+
+
+
+/* More and announcements footer and search top */
+li.more a, a .read-more, .form-submit button, .form-submit input {
+background:  #c00000; 
+}
+li.more a:hover, a .read-more:hover, .form-submit button:hover, .form-submit input:hover {
+background: #363f48; 
+}
+.announce {
+background: #c00000; 
+}
+.announce.social-header {
+background: #990000; 
+}
+footer {
+background: #990000;
+}
+
+/* Main Content Area */
+/* Flexslider navigation buttons */
+.flex-direction-nav a {
+background: #c00000;
+}
+
+
+
+
+
+header nav {
+	background: #000000;
+		background: rgba(0,0,0,0.5);
+	}
+
+
+
+header nav ul ul {
+background-color: #ffffff}
+
+.mean-container .mean-bar, .mean-container .mean-nav {
+background-color: #363f48}
+
+
+/* Font Colours */
+/* Main body font colour */
+body, 
+input, 
+button,
+textarea,
+.home.page-template-template-portfolio-php .the-content {
+color: #363f48; 
+}
+
+/* Links */
+a {
+color: #c00000;
+}
+a:hover {
+color: #363f48;
+}
+
+/* Site title */
+.site-title, .site-title a {
+color: #ffffff;
+}
+.site-title a:hover {
+color: #c00000;
+}
+.tagline {
+color: #ffffff;
+}
+.announce {
+color: #ffffff;
+}
+.announce a {
+color: #ffffff;
+}
+.announce a:hover {
+color: #ffffff;
+}
+.meta {
+color: #363f48;
+}
+.meta a {
+color: #363f48;
+}
+.meta a:hover {
+color: #c00000;
+}
+.pagination a {
+color: #fafafa;
+}
+.pagination a:hover {
+color: #363f48;
+}
+footer {
+color: #ffffff;
+}
+footer a {
+color: #ffffff;
+}
+footer a:hover {
+color: #363f48;
+}
+.sidebar {
+color: #363f48;
+}
+.sidebar a {
+color: #c00000;
+}
+.sidebar a:hover {
+color: #363f48;
+}
+li.more a, a .read-more, .form-submit button, .form-submit input {
+color: #ffffff;
+}
+li.more a:hover, a .read-more:hover, .form-submit button:hover, .form-submit input:hover {
+color: #ffffff;
+}
+h1 {
+color: #c00000;
+}
+.archive-content h2 a {
+color: #c00000;
+}
+.archive-content h2 a:hover {
+color: #363f48;
+}
+
+/* Post format colours */
+article.format-link .the-content h2 a, .home article.format-link .the-content h2 a, article.format-aside .the-content h2 a, .home article.format-aside .the-content h2 a, article.format-link .the-content h2 a, .home article.format-aside .the-content, article.format-link .the-content, .archive-content article.format-status .the-content {
+color: #363f48;
+}
+article.format-link .the-content h2 a, article.format-link .the-content h1 a, .article-content article.format-link .the-content h2 a, .home article.format-link .the-content h2 a, .archive article.format-link .the-content h2 a {
+color: #c00000;
+}
+/* Header */
+header nav a {
+color: #ffffff;
+}
+
+header nav ul li.sfHover a,
+header nav ul li a:hover, 
+header nav li.current_page_item a,
+header nav li.current-menu-item a,
+header nav li.current_page_ancestor a, 
+header nav li.current_page_parent a,
+header nav li.current-post-ancestor a, 
+header nav li.current-page-ancestor a {
+color: #c00000;
+}
+.archive-content article .post-content .meta.bottom, .navigation, .comment-system, .contact-form, .meta.top li, .single .meta.bottom, header nav, .time a, .navigation a, .pagination li a, .pagination li span {
+border-color: #e1e1e1}
+.webicon {
+color: #afafaf}
+header nav ul ul li a, header nav li.sfHover li a {
+color: #363d48;
+}
+h1.searching span, .highlight {
+background-color: #fbf8ce;
+}
+.portfolio-content {
+background-color: #363f48;
+}
+
+header nav {
+	font-size: 18px}
+
+/* Main body font size */
+body, 
+input, 
+button,
+textarea {
+font-size: 20px;
+}
+.meta, .announce, footer {
+font-size: 15px;
+}
+h1, .archive-content h2, .archive-content h2 a {
+font-size: 39px;
+}
+h2 {
+font-size: 30px;
+}
+h3 {
+font-size: 26px;
+}
+h4 {
+font-size: 24px;
+}
+h5, .portfolio-content h2 {
+font-size: 22px;
+}
+h6 {
+font-size: 21px;
+}
+.meta li.more, .time a, .navigation a, .pagination li a, .pagination li span, .comment-reply a, a .read-more {
+font-size: 12px;
+}
+.sidebar h5 {
+font-size: 20px;
+}
+.site-title {
+font-size: 33px;
+}
+
+header .logo {
+text-align: center;
+}
+
+
+@media screen and (max-width: 780px) {
+header nav {
+display: none;
+}
+}
+
+	
+header nav {background: rgba(0,0,0,0.3)} .tagline {font-family: Helvetica,Arial,sans-serif,"cursive"; font-weight:bold} blockquote {font-size: 100%; text-align: left; border-width: 0px medium; padding-top: 0em; padding-bottom: 0em;} h2 {color: #c00000; font-weight: 800} h3 {color: #c00000} footer a {border-bottom: 0px solid} footer a:hover {border-bottom: 0px solid} .archive-content article { margin-top: 0; margin-bottom: 0; } .navigation > ul > li ul {top:50px} .navigation > ul > li > ul li ul {margin-left:0}
+
+	
+	
